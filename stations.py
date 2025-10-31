86c24cbde84a9ca156c8c2ea02a80adf39275bac
Oladejo Mariam Opeyemi <113688344+Qweenfumzy20@users.noreply.github.com>
2025-10-31 01:17:27 +0000

    Implement hash table for operational stations
    Obtain the list of unique stations from the dataset
    Added functionality to populate a hash table with unique station names and check if a specific station is operational.

--- stations.py
+++ stations.py
@@ -4,5 +4,22 @@ stations = pd.read_excel('London_stations.xlsx', header=None)
 
 unique_stations = stations[1].unique().tolist()
 
-print(unique_stations)
+print('Our unique stations are: ', unique_stations)
 
+
+
+# List of station names from the dataset
+stations = unique_stations
+
+# Initializing the hash table (set in this case)
+operational_stations = set()
+
+# Populating the hash table with station names
+for station in stations:
+    operational_stations.add(station)
+
+# Example of querying the hash table to check if a station is operational
+query_station = 'Acton Main Line'
+is_operational = query_station in operational_stations
+
+print(f'Is {query_station} operational? {is_operational}')
